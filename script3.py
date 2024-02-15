6dc86d449e3ea4165efe2ca16442ad647adfc7b2
renediKazenza <rkazenza@gmail.com>
2024-02-15 14:42:21 +0100

    creation d'un script

---
+++ script3.py
@@ -0,0 +1,8 @@
+import os
+def new_user():
+    confirm="N"
+    while confirm !="Y":
+        username = input("indiquez le nom de l'utilisateur à ajouter: ")
+        print("utliser username '" +username + "'? (Y/N)")
+        confirm = input().upper()
+        os.system("sudo adduser " + username)
